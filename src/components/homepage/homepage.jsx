60cfa7dde19b3025e2fedc4661fb0d5c3a32b54d
Yifan Zhang <freqyifan@gmail.com>
2018-05-09 09:35:36 +0300

    clean up

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -4,65 +4,10 @@ import PropTypes from 'prop-types';
 import DocumentTitle from 'react-document-title';
 import { Grid, Header, Button, Icon } from 'semantic-ui-react';
 import Footer from '../base/footer';
-import NewsItem from './newsitem';
-import MajorNewsItem from './majornewsitem';
-import TextNewsItem from './textnewsitem';
 
 const buttonGroupStyle = {
   width: '100%',
 };
-
-const playButtonStyle = {
-  width: '50%',
-};
-
-const newsItemContainerStyle = {
-  width: '100%',
-};
-
-const majorNewsItemData = {
-  left: [{
-    imageUrl: 'https://ichef.bbci.co.uk/news/695/cpsprodpb/17525/production/_100252559_52985792-d2c0-4b9a-92c1-2837a5f74356.jpg',
-    title: 'Trading partners threaten retaliation',
-    snippet: 'The main trading partners of the US have reacted angrily after President Donald Trump announced plans to impose tariffs on steel and aluminium imports.',
-    date: '2 hours ago',
-    source: 'news.bbc.co.uk',
-  }, {
-    imageUrl: 'https://files.qatarliving.com/styles/horizontal_large/s3/post/2018/03/01/fake%20news%201.jpg?7zsm1U7.j_ffO03xnAPfCm3S1q3v7SRh&itok=oOLzEng_',
-    title: 'Trading partners threaten retaliation',
-    snippet: 'The main trading partners of the US have reacted angrily after President Donald Trump announced plans to impose tariffs on steel and aluminium imports.',
-    date: '2 hours ago',
-    source: 'news.bbc.co.uk',
-  }],
-  right: [{
-    imageUrl: 'https://files.qatarliving.com/styles/horizontal_large/s3/post/2018/03/01/fake%20news%201.jpg?7zsm1U7.j_ffO03xnAPfCm3S1q3v7SRh&itok=oOLzEng_',
-    title: 'US investment giant pressures gun firms',
-    snippet: 'The main trading partners of the US have reacted angrily after President Donald Trump announced plans to impose tariffs on steel and aluminium imports.',
-    date: '2 hours ago',
-    source: 'news.bbc.co.uk',
-  }, {
-    imageUrl: 'https://ichef.bbci.co.uk/news/695/cpsprodpb/17525/production/_100252559_52985792-d2c0-4b9a-92c1-2837a5f74356.jpg',
-    title: 'US investment giant pressures gun firms',
-    snippet: 'The main trading partners of the US have reacted angrily after President Donald Trump announced plans to impose tariffs on steel and aluminium imports.',
-    date: '2 hours ago',
-    source: 'news.bbc.co.uk',
-  }],
-};
-
-const newsItemData = [{
-    imageUrl: 'https://ichef.bbci.co.uk/news/736/cpsprodpb/6FD0/production/_100242682_gettyimages-906344120.jpg',
-    title: 'Trump steel tariffs: Trading partners threaten retaliation',
-    snippet: 'The main trading partners of the US have reacted angrily after President Donald Trump announced plans to impose tariffs on steel and aluminium imports.',
-    date: '2 hours ago',
-    source: 'news.bbc.co.uk',
-  }, {
-    imageUrl: 'https://ichef-1.bbci.co.uk/news/736/cpsprodpb/131F4/production/_100242387_gettyimages-925805956.jpg',
-    title: 'Snow in Europe: Deadly winter storm brings chaos',
-    snippet: 'Large parts of Europe have endured another night of freezing conditions as a Siberian weather system continues to bring chaos.',
-    date: '3 hours ago',
-    source: 'news.bbc.co.uk',
-  },
-];
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -70,8 +15,6 @@ class HomePage extends React.Component {
   }
 
   render() {
-    const newsitems = newsItemData.map((d, i) => <Grid.Column key={`item-${i}`}> <NewsItem {...d}/> </Grid.Column>);
-
     return (
       <div>
         <Header textAlign='center'>
@@ -83,16 +26,6 @@ class HomePage extends React.Component {
             </Button>
           </Button.Group>
         </Header>
-        <Grid textAlign='center' columns={1} padded style={{position:'relative'}} celled>
-          {newsitems[0]}
-          <Grid.Column>
-            <MajorNewsItem {...majorNewsItemData}/>
-          </Grid.Column>
-          {newsitems[1]}
-          <Grid.Column>
-            <TextNewsItem/>
-          </Grid.Column>
-        </Grid>
         <Footer/>
       </div>
     );
